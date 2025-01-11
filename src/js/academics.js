edcabefa7dd490a7ecc34ab210e6ab26406daed1
Jaelouss <devilsbrother1@gmail.com>
2025-01-11 23:16:24 +0200

    header done

--- src/js/academics.js
+++ src/js/academics.js
@@ -1,2 +1,51 @@
 console.log('Welcome to the School Project! Academics');
 import '../scss/academics.scss';
+
+class Header {
+	selectors = {
+		root: '[data-js-header]',
+		overlay: '[data-js-header-overlay]',
+		burgerButton: '[data-js-header-burger-button]',
+	};
+
+	stateClasses = {
+		isActive: 'is-active',
+		isLock: 'is-lock',
+	};
+
+	constructor() {
+		this.rootElement = document.querySelector(this.selectors.root);
+		this.overlayElement = this.rootElement.querySelector(
+			this.selectors.overlay
+		);
+		this.burgerButtonElement = this.rootElement.querySelector(
+			this.selectors.burgerButton
+		);
+		this.bindEvents();
+	}
+
+	onBurgerButtonClick = () => {
+		this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
+		this.overlayElement.classList.toggle(this.stateClasses.isActive);
+		document.documentElement.classList.toggle(this.stateClasses.isLock);
+	};
+
+	bindEvents() {
+		this.burgerButtonElement.addEventListener(
+			'click',
+			this.onBurgerButtonClick
+		);
+	}
+}
+
+new Header();
+
+document.addEventListener('DOMContentLoaded', () => {
+	// Вибираємо всі SVG
+	const svgs = document.querySelectorAll('.icon--svg-header');
+
+	// Додаємо анімацію
+	svgs.forEach(svg => {
+		svg.classList.add('animate');
+	});
+});
