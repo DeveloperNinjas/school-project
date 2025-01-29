174b4e79ce4e38aa75e0cabf483e09c61802b84e
Jaelouss <devilsbrother1@gmail.com>
2025-01-29 17:02:49 +0200

    add contact section, add js logic for form, change logic for header and icon hide

--- src/js/admission.js
+++ src/js/admission.js
@@ -41,18 +41,8 @@ class Header {
 new Header();
 
 document.addEventListener('DOMContentLoaded', () => {
-	const svgs = document.querySelectorAll('.icon--svg-header');
-
-	svgs.forEach(svg => {
-		svg.classList.add('animate');
-	});
-});
-
-document.addEventListener('DOMContentLoaded', () => {
 	const header = document.querySelector('.header');
-	const icons = document.querySelectorAll('.icon--svg-special');
 	let lastScrollY = window.scrollY;
-	let animatedIcons = new Set();
 
 	window.addEventListener('scroll', () => {
 		const currentScrollY = window.scrollY;
@@ -62,16 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		} else if (currentScrollY === 0) {
 			header.classList.remove('hidden');
 		}
-
-		icons.forEach((icon, index) => {
-			if (animatedIcons.has(index)) return;
-
-			const rect = icon.getBoundingClientRect();
-			if (rect.top < window.innerHeight && rect.bottom > 0) {
-				icon.classList.add('is-visible');
-				animatedIcons.add(index);
-			}
-		});
 
 		lastScrollY = currentScrollY;
 	});
